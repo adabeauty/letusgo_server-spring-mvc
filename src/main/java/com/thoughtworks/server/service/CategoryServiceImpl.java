d2ad8c871cbffe26160e85332226e8f1d3da46e3
adabeauty <654296217@qq.com>
2014-11-14 11:38:10 +0800

    modify getCategories and getCategoryById method

--- src/main/java/com/thoughtworks/server/service/CategoryServiceImpl.java
+++ src/main/java/com/thoughtworks/server/service/CategoryServiceImpl.java
@@ -1,16 +1,23 @@
 package com.thoughtworks.server.service;
 
 import com.thoughtworks.server.dao.CategoryDao;
+import com.thoughtworks.server.dao.ItemDao;
 import com.thoughtworks.server.model.Category;
+import com.thoughtworks.server.model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
     @Autowired
     private CategoryDao categoryDaoImpl;
+
+    @Autowired
+    private ItemDao itemDaoImpl;
 
     public void setCategoryDaoImpl(CategoryDao categoryDaoImpl) {
         this.categoryDaoImpl = categoryDaoImpl;
@@ -18,7 +25,10 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public Category getCategoryById(int id) {
-        return categoryDaoImpl.getCategoryById(id);
+        List<Item> item= itemDaoImpl.getItemsByCategoryId(id);
+        Category category = categoryDaoImpl.getCategoryById(id);
+        System.out.println(new Category(category.getId(), category.getName(), item.size()));
+        return new Category(category.getId(), category.getName(), item.size());
     }
 
     @Override
@@ -38,6 +48,12 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public List<Category> getCategories() {
-        return categoryDaoImpl.getCategories();
+        List<Category> categoryList = new ArrayList<Category>();
+        List<Category> categories = categoryDaoImpl.getCategories();
+        for(int i = 0; i<categories.size(); i++){
+            List<Item> items = itemDaoImpl.getItemsByCategoryId(categories.get(i).getId());
+            categoryList.add(new Category(categories.get(i).getId(), categories.get(i).getName(), items.size()));
+        }
+        return categoryList;
     }
 }
