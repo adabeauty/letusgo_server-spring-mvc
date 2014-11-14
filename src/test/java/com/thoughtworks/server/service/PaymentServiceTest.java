979fc83efe8d624e3353b4143962dff891aeffc2
adabeauty <654296217@qq.com>
2014-11-14 20:33:55 +0800

    finish deleteCartItems of paymentService test

--- src/test/java/com/thoughtworks/server/service/PaymentServiceTest.java
+++ src/test/java/com/thoughtworks/server/service/PaymentServiceTest.java
@@ -1,7 +1,30 @@
 package com.thoughtworks.server.service;
 
-/**
- * Created by wanghuan on 14-11-14.
- */
+import com.thoughtworks.server.dao.CartItemDao;
+import com.thoughtworks.server.dao.CartItemDaoImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+
 public class PaymentServiceTest {
+    PaymentService paymentServiceImpl;
+    CartItemDao cartItemDaoImpl;
+
+    @Before
+    public void mock_paymentDao(){
+        cartItemDaoImpl = mock(CartItemDaoImpl.class);
+
+        paymentServiceImpl= new PaymentServiceImpl();
+        ReflectionTestUtils.setField(paymentServiceImpl, "cartItemDaoImpl", cartItemDaoImpl);
+    }
+
+    @Test
+    public void can_delete_all_cartItems(){
+        paymentServiceImpl.deleteCartItems();
+        verify(cartItemDaoImpl).deleteCartItems();
+    }
 }
